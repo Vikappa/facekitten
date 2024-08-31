49916f05dfe4084e3c9faee1430f3d9fe63d096a
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-08-31 17:25:47 +0200

    NavBar added into homepage

---
+++ src/app/organisms/HomePage.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar"
+
+const HomePage = (
+    {data}: {data: any}
+) => {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default HomePage
