e324c64556e140c65aa94799c553a8dd8346e6a5
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-08-31 17:23:16 +0200

    Homepage shown based on localStorage state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
 'use client'
 
+import HomePage from './organisms/HomePage';
 import LoginPage from './organisms/LoginPage';
 import './style.css';
 
 export default function Home() {
 
+  let localStorageData = localStorage.getItem('facekittenData');
 
-  return (
-    <main className={'container py-5'}>
+  try{
+    localStorageData = localStorage.getItem('facekittenData');
+  } catch(error){
+    console.log("Localstorage non trovato, presento LoginPage")
+  }
+
+  if (!localStorageData){
+
+    return (
+      <main className={'container py-5 bg-grayBg'}>
       <LoginPage/>
     </main>
   );
+} else {
+  return (
+    <HomePage data={localStorageData}/>
+  )
+} 
 }
