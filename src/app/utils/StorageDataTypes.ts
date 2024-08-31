54f29e75714f80c1ae3cb1c71a9590075b4bb755
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-01 00:19:33 +0200

    storageData type update

--- src/app/utils/StorageDataTypes.ts
+++ src/app/utils/StorageDataTypes.ts
@@ -13,6 +13,11 @@ export interface Friend {
     lastMessageStatus: string;
 }
 
+export interface UserDetails{
+    name: string;
+    profilepicture:string
+}
+
 export interface Message {
     id: number;
     sender: string;
@@ -46,4 +51,38 @@ export interface ChatList {
 export interface storageData {
     friends: FriendList[];
     chats: ChatList[];
+    userDetails: UserDetails
+}
+
+export interface pexelPayload {
+    page:          number;
+    per_page:      number;
+    photos:        PhotoPayload[];
+    total_results: number;
+    next_page:     string;
+}
+
+export interface PhotoPayload {
+    id:               number;
+    width:            number;
+    height:           number;
+    url:              string;
+    photographer:     string;
+    photographer_url: string;
+    photographer_id:  number;
+    avg_color:        string;
+    src:              photoSrc;
+    liked:            boolean;
+    alt:              string;
+}
+
+export interface photoSrc {
+    original:  string;
+    large2x:   string;
+    large:     string;
+    medium:    string;
+    small:     string;
+    portrait:  string;
+    landscape: string;
+    tiny:      string;
 }
