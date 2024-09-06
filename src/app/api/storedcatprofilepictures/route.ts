a6753bc4b7bb3a6985d6ee9c1f3a1d9fc16b58e9
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-06 13:51:08 +0200

    opt out cache

--- src/app/api/storedcatprofilepictures/route.ts
+++ src/app/api/storedcatprofilepictures/route.ts
@@ -1,10 +1,10 @@
-import { pexelPayload } from '@/app/utils/StorageDataTypes';
-import { NextRequest, NextResponse } from 'next/server';
-import { picIndexes } from '../../../../public/storedcatprofilepictures/imgs/fileindexer'
+import { NextResponse } from 'next/server';
+import { picIndexes } from '../../../../public/storedcatprofilepictures/imgs/fileindexer';
+
+export const dynamic = 'force-dynamic';
 
 export function GET() {
+  const randomPic = picIndexes[Math.floor(Math.random() * picIndexes.length)];
 
-    const randomPic = picIndexes[Math.floor(Math.random() * picIndexes.length)]
-
-    return NextResponse.json(randomPic)
+  return NextResponse.json(randomPic);
 }
