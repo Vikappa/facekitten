dcca8263f4fb198b086b62c93b3f8fcddddc81ae
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-09 16:10:52 +0200

    covers files added, index updated

--- public/storedcatcoverphotos/coverPhotoIndexes.ts
+++ public/storedcatcoverphotos/coverPhotoIndexes.ts
@@ -1,3 +1,9 @@
 export const coverPhotoIndexes = [
     '/storedcatcoverphotos/imgs/199dda88fbca22eae7b3c5f3c022e16b.jpg',
+    '/storedcatcoverphotos/imgs/2a2148f68d29e2f8177528df8c2064b3--cover-photos-for-facebook-facebook-timeline.jpg',
+    '/storedcatcoverphotos/imgs/Beautiful-Black-Cat-Facebook-Covers-FBcoverlover_facebook_cover.jpg',
+    '/storedcatcoverphotos/imgs/Cat-Sunglasses-Facebook-Covers-1670.jpg',
+    '/storedcatcoverphotos/imgs/Cool-Cat-Facebook-Profile-Timeline-Cover-Ultimate-Collection-Of-Top-50-Best-Cute-Facebook-Cover-For1.jpg',
+    '/storedcatcoverphotos/imgs/d8d1b4edc273553a807e856aca7ac1b6.jpg',
+    '/storedcatcoverphotos/imgs/grumpy-cat-background-facebook-cover.jpg',
 ];
