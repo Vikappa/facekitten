3bae6a9342177fcf4361a43d1be861aaca22e3d5
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-09 12:54:33 +0200

    removed navbar padding

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -7,7 +7,14 @@ import MobileProfileSection from "./MobileProfileSection"
 import { useAppDispatch, useAppSelector, useAppStore } from "../lib/hooks"
 import { setNavbarPage } from '../lib/slices/appStateSlice'
 import MobileOptNavbar from "./MobileOptNavbar"
+import { useEffect, useRef, useState } from "react"
 const NavBar = () => {
+
+    const notificationLength = useAppSelector(state => state.notifications.notifications.flatMap(notification => notification.seen));
+        const [prevLength, setPrevLength] = useState(notificationLength.length);
+
+        const audioRef1 = useRef<HTMLAudioElement>(null);
+        const audioRef2 = useRef<HTMLAudioElement>(null);
 
         const dispatch = useAppDispatch();
         const navbarPage = useAppSelector(state => state.status.shownpage)
@@ -16,8 +23,15 @@ const NavBar = () => {
             dispatch(setNavbarPage(page));
         }
 
+        useEffect(() => {
+            if (notificationLength.length > prevLength) {
+                audioRef1.current?.play();
+            }
+            setPrevLength(notificationLength.length);
+        }, [notificationLength.length, prevLength]);
+
     return(
-    <div className="d-flex flex-column justify-content-center align-items-center bg-white shadow-sm p-1 px-3 w-100">
+    <div className="d-flex flex-column justify-content-center align-items-center bg-white shadow-sm w-100">
 
     <div className="d-flex justify-content-between align-items-center bg-white shadow-sm p-1 px-3 w-100">
         <DesktopSearchbar/>
@@ -32,6 +46,8 @@ const NavBar = () => {
     <div className="d-sm-none">
         <MobileOptNavbar />
     </div>
+    <audio ref={audioRef1} src="/assets/notificationEffect.m4a" />
+    <audio ref={audioRef2} src="/assets/notificationEffect.m4a" />
 
     </div>
 
