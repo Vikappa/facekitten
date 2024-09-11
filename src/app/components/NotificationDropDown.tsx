321dc3cd8a3ca415427bda03242f9a359a0fb605
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-11 15:43:17 +0200

    fix zindex notification dropdown

--- src/app/components/NotificationDropDown.tsx
+++ src/app/components/NotificationDropDown.tsx
@@ -17,7 +17,7 @@ const NotificationDropDown = () => {
     }, [show])
     
     return (
-        <div className={`${show ? `d-block` : `d-none`} bg-white text-black position-absolute pe-3 py-2 rounded-2 shadow`} style={{minWidth:'30vw',zIndex: 1000, right: '1vh', boxShadow: '4px -2px 8px rgba(0, 0, 0, 0.1) !important'}}>
+        <div className={`${show ? `d-block` : `d-none`} bg-white text-black position-absolute pe-3 py-2 rounded-2 shadow`} style={{minWidth:'30vw',zIndex: 2000, right: '1vh', boxShadow: '4px -2px 8px rgba(0, 0, 0, 0.1) !important'}}>
             <ol className="p-0 m-0 px-3" style={{fontSize:'0.8rem'}}>
             {notifications.toReversed().map(notification => (
                     <NotificationLi key={notification.id} notification={notification} />
