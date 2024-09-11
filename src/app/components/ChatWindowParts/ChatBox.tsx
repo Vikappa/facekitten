c72db39781c175fd2bb6f8ee6d66fb4e5ef5264a
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-11 11:08:29 +0200

    fix scrollable chatbox

--- src/app/components/ChatWindowParts/ChatBox.tsx
+++ src/app/components/ChatWindowParts/ChatBox.tsx
@@ -2,30 +2,43 @@
 
 import { Chat } from "@/app/utils/StorageDataTypes"
 import MessageBoxLi from "./MessageBoxLi"
-import { useEffect, useState } from "react"
+import { useEffect, useRef } from "react"
 import StaScrivendo from "@/app/atoms/StaScrivendo"
 
 const ChatBox = (
     {chat, staScrivendo}: 
-    {chat:Chat; staScrivendo:boolean}
+    {chat: Chat; staScrivendo: boolean}
 ) => {
+    const chatContainerRef = useRef<HTMLDivElement | null>(null);
 
-    
+    const scrollToBottom = () => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+    };
 
-    return(
+    useEffect(() => {
+        scrollToBottom();
+    }, [chat.messages]);
+
+    return (
         <div
-        style={{
-            minHeight:'25vh'
-        }}
-        className="d-flex flex-column justify-content-end py-2 bg-white"
+            ref={chatContainerRef} 
+            style={{
+                minHeight: '25vh',
+                maxHeight: '25vh',
+                backgroundColor: 'white',
+                overflowY: 'auto',
+                paddingBottom:'6px' 
+            }}
         >
             {chat.messages.map((message, index) => (
                 <MessageBoxLi key={index} message={message} />
             ))}
-            
+
             {staScrivendo && <StaScrivendo staScrivendo={staScrivendo} />}
         </div>
-    )
+    );
 }
 
-export default ChatBox
+export default ChatBox;
