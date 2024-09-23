52dca454aeac040ffbf4f0f333fcd34637cbd171
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-23 17:13:55 +0200

    update comment link

--- src/app/atoms/PostCommentLi.tsx
+++ src/app/atoms/PostCommentLi.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image"
 import { PostComment } from "../utils/StorageDataTypes"
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import Link from "next/link";
 
 const PostCommentLi = ({comment}: {comment: PostComment}) => {
 
@@ -9,9 +10,11 @@ const PostCommentLi = ({comment}: {comment: PostComment}) => {
     <div className="d-flex align-items-center gap-2 pt-2 position-relative">
         <Image src={comment.author.profilepicture} alt={comment.author.userName} width={34} height={34} className="rounded-circle" />
         <div className="d-flex flex-column rounded-4 p-1 px-2 bg-grayBg ">
-        <p className="m-0 px-1 fw-bold">
+        <Link href={`/userprofile/${comment.author.userName}`} className="text-decoration-none text-dark fw-bold">
+        <p className="m-0 px-1 ">
             {comment.author.userName}
         </p>
+        </Link>
         <p className="m-0 px-1">
             {comment.body}
         </p>
