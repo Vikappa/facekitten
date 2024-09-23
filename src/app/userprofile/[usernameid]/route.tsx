a6eb0eb2a3fec935706c1fc1c85aa1313adfff0c
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-23 13:19:36 +0200

    userprofile bot page created

---
+++ src/app/userprofile/[usernameid]/route.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useParams } from 'next/navigation'; // Importa useParams
+import '../style.css';
+import Navbar from "../../components/NavBar";
+import ModaleNotificationMobileModale from "../../modali/ModaleNotificationMobileModale";
+import UserProfileRenderer from "../../organisms/UserProfileRenderer";
+import MobileOptionFullScreenModal from '../../modali/MobileOptionFullScreenModal';
+
+const UserBotPage = () => {
+    const { usernameid } = useParams()
+
+    return (
+        <div id="container bg-danger">
+            <ModaleNotificationMobileModale />
+            <MobileOptionFullScreenModal />
+            <Navbar />
+            <h1>Profilo utente: {usernameid}</h1> 
+        </div>
+    );
+}
+
+export default UserBotPage;
