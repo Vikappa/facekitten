3f16bf3fb7ca3f88924ef095844745ababf409f9
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-23 23:13:20 +0200

    file rename, index ts file

---
+++ public/storedcatvideos/video/postVideosIndexes.ts
@@ -0,0 +1,7 @@
+export const postImgIndexes = [
+    '/storedcatvideos/video/Fears of Sonyas cat.mp4',
+    '/storedcatvideos/video/The_Savitsky_Cats_Super_Trained_Cats_Perform_Exciting_Routine_-_Americas_Got_Talent_2018.mp4',
+    '/storedcatvideos/video/videoplayback (1).mp4',
+    '/storedcatvideos/video/videoplayback (2).mp4',
+    '/storedcatvideos/video/videoplayback.mp4',
+];
