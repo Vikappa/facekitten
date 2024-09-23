0afa0c80bf2483c9f7ccbf90c47688f18beeac3e
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-23 21:59:09 +0200

    update post navigation

--- src/app/userpost/[id]/page.tsx
+++ src/app/userpost/[id]/page.tsx
@@ -1,7 +1,23 @@
-const UserPostPage = () => {
+'use client'
 
+import NavBar from "@/app/components/NavBar"
+import { useAppSelector } from "@/app/lib/hooks"
+import SinglePostPageOrg from "@/app/organisms/SinglePostPageOrg"
+import { useParams } from "next/navigation"
+import '@/app/style.css'
+
+
+const UserPostPage = () => {
+    const id = useParams()
+    const post = useAppSelector(state => state.posts.userPosts.find(post => post.id === Number(id.id)))
+
+    if(!post){
+        return null
+    }   
     return(
         <>
+        <NavBar/>
+        <SinglePostPageOrg post={post}/>
         </>
     )
 }
--- src/app/userprofile/[usernameid]/[postid]/page.tsx
+++ src/app/userprofile/[usernameid]/[postid]/page.tsx
@@ -1,23 +1,27 @@
 'use client'
 
+import NavBar from "@/app/components/NavBar"
 import { useAppSelector } from "@/app/lib/hooks"
 import MobileOptionFullScreenModal from "@/app/modali/MobileOptionFullScreenModal"
 import ModaleNotificationMobileModale from "@/app/modali/ModaleNotificationMobileModale"
 import SinglePostPageOrg from "@/app/organisms/SinglePostPageOrg"
 import { useParams } from "next/navigation"
-
+import '@/app/style.css'
 const UserBotPostPage = () => {
-
+    
     const {author, postid} = useParams()
-    const post = useAppSelector(state => state.sessionGeneratedAccounts.acc.find(account => account.name === author)?.posts.find(post => post.id === Number(postid)))
-
+    const post = useAppSelector(state => state.sessionGeneratedAccounts.acc.flatMap(acc => acc.posts).find(post => post.id === Number(postid)))
+    const posts = useAppSelector(state => state.sessionGeneratedAccounts.acc)
     if(!post){
+        console.log(postid)
+        console.log(posts)
         return null
     }
     return (
         <div>
             <ModaleNotificationMobileModale />
             <MobileOptionFullScreenModal />
+            <NavBar/>
             <SinglePostPageOrg post={post}/>
         </div>
     )
