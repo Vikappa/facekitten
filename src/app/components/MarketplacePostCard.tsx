4eefd13d4cb6c28dfcfe2d3ddc525737b25c2907
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-23 23:11:52 +0200

    directory update, scss update

--- src/app/components/MarketplacePostCard.tsx
+++ src/app/components/MarketplacePostCard.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image"
 import { Post } from "../utils/StorageDataTypes"
+import Link from "next/link"
 
 const MarketplacePostCard = ({post}:{post: Post}) => {
 
@@ -11,9 +12,11 @@ const MarketplacePostCard = ({post}:{post: Post}) => {
             <div className="card mb-3 col-6 col-md-2 p-1 bg-transparent border-0 d-flex flex-column">
                 <Image src={post.body.marketplacePhotoUrl} alt="Marketplace Post Image" className="card-img-top rounded-3" width={200} height={200} />
                 <div className="p-1">
+                    <Link href={`/userprofile/${post.author.userName}/${post.id}`} className="text-decoration-none text-black underline-hover">
                     <p className="p-0 m-0 fs-5 fw-semibold">{post.body.marketplacePrice}</p>
                     <p className="p-0 m-0 fs-5 fw-semibold">{post.body.marketplaceTitle}</p>
-                    <p className=""
+                    </Link>
+                    <p
                     style={{
                         color: 'grey',
                         fontSize: '0.9rem',
