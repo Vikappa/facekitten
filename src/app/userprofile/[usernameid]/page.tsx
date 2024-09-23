449c47c1e3c91017528dfafb77a3013bea88cd8e
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-23 13:39:44 +0200

    username decoded

--- src/app/userprofile/[usernameid]/page.tsx
+++ src/app/userprofile/[usernameid]/page.tsx
@@ -1,21 +1,24 @@
 'use client'
 import { useParams } from 'next/navigation'
-import '../../style.css';
-import Navbar from "../../components/NavBar";
-import ModaleNotificationMobileModale from "../../modali/ModaleNotificationMobileModale";
-import UserProfileRenderer from "../../organisms/UserProfileRenderer";
-import MobileOptionFullScreenModal from '../../modali/MobileOptionFullScreenModal';
-import { useAppDispatch } from '@/app/lib/hooks';
-import { useEffect } from 'react';
-import { setNavbarPage } from '@/app/lib/slices/appStateSlice';
+import '../../style.css'
+import Navbar from "../../components/NavBar"
+import ModaleNotificationMobileModale from "../../modali/ModaleNotificationMobileModale"
+import UserProfileRenderer from "../../organisms/UserProfileRenderer"
+import MobileOptionFullScreenModal from '../../modali/MobileOptionFullScreenModal'
+import { useAppDispatch } from '@/app/lib/hooks'
+import { useEffect } from 'react'
+import { setNavbarPage } from '@/app/lib/slices/appStateSlice'
 
 const UserBotPage = () => {
-    const { usernameid } = useParams()
+    const params = useParams()
+    const usernameid = Array.isArray(params.usernameid) ? params.usernameid[0] : params.usernameid
+    const userNameString = decodeURIComponent(usernameid || '')
+
     const dispatch = useAppDispatch()
     
     useEffect(() => {
         dispatch(setNavbarPage(20))
-    }, [])
+    }, [dispatch])
     
 
     return (
@@ -23,9 +26,8 @@ const UserBotPage = () => {
             <ModaleNotificationMobileModale />
             <MobileOptionFullScreenModal />
             <Navbar />
-            <h1>Profilo utente: {usernameid}</h1> 
+            <h1>Profilo utente: {userNameString}</h1> 
         </div>
-    );
+    )
 }
-
-export default UserBotPage;
+export default UserBotPage
