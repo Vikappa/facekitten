be49ec074343a9b8b435da6ec9424046cf46a521
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-23 13:37:05 +0200

    fix  navbar status when visiting bots profilepages

--- src/app/userprofile/[usernameid]/page.tsx
+++ src/app/userprofile/[usernameid]/page.tsx
@@ -5,9 +5,18 @@ import Navbar from "../../components/NavBar";
 import ModaleNotificationMobileModale from "../../modali/ModaleNotificationMobileModale";
 import UserProfileRenderer from "../../organisms/UserProfileRenderer";
 import MobileOptionFullScreenModal from '../../modali/MobileOptionFullScreenModal';
+import { useAppDispatch } from '@/app/lib/hooks';
+import { useEffect } from 'react';
+import { setNavbarPage } from '@/app/lib/slices/appStateSlice';
 
 const UserBotPage = () => {
     const { usernameid } = useParams()
+    const dispatch = useAppDispatch()
+    
+    useEffect(() => {
+        dispatch(setNavbarPage(20))
+    }, [])
+    
 
     return (
         <div id="container bg-danger">
