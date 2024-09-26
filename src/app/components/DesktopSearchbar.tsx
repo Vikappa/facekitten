4e54cb4caacf54a4e3262e7da6344fa111d277cb
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-26 12:34:23 +0200

    now navbar icon leads to homepage

--- src/app/components/DesktopSearchbar.tsx
+++ src/app/components/DesktopSearchbar.tsx
@@ -3,15 +3,27 @@ import Image from "next/image"
 import { useState } from "react";
 import { Form } from "react-bootstrap"
 import { CiSearch } from "react-icons/ci";
+import { useAppDispatch } from "../lib/hooks";
+import { setNavbarPage, setShowDropDownNotification, setShowProfileDropDown } from "../lib/slices/appStateSlice";
+import { useRouter } from "next/navigation";
 
 const DesktopSearchbar = () => {
 
     const [searchValue, setSearchValue] = useState('')
-    
+    const dispatch = useAppDispatch()
+    const router = useRouter()
+    const setPage1 = () => {
+        dispatch(setShowDropDownNotification(false))
+        dispatch(setShowProfileDropDown(false))
+        router.push('/')
+        setNavbarPage(1)
+    }
 
     return(
         <div className="d-none d-sm-flex align-items-center gap-3">
-        <Image src={'/img/facekittenlogo.png'} alt="Logo" width={50} height={50}/>
+        <Image src={'/img/facekittenlogo.png'} alt="Logo" width={50} height={50} onClick={setPage1} style={{
+            cursor:'pointer'
+        }}/>
         {(searchValue.length === 0 ) && <CiSearch className="position-absolute" style={{transform:'translate(300%,0)'}} size={25}/>}
         <Form className="d-none d-sm-block">
             <Form.Control type="text" 
