33b18e3b71701be176e5c5a2a135e1c57e6dbfe9
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-30 16:53:22 +0200

    fix page hidratating

--- src/app/userprofile/[usernameid]/page.tsx
+++ src/app/userprofile/[usernameid]/page.tsx
@@ -1,52 +1,73 @@
-'use client'
-import { useParams, useRouter } from 'next/navigation'
-import '../../style.css'
-import Navbar from "../../components/NavBar"
-import ModaleNotificationMobileModale from "../../modali/ModaleNotificationMobileModale"
-import MobileOptionFullScreenModal from '../../modali/MobileOptionFullScreenModal'
-import { useAppDispatch, useAppSelector } from '@/app/lib/hooks'
-import { useEffect } from 'react'
-import { setNavbarPage } from '@/app/lib/slices/appStateSlice'
-import BotProfileRenderer from '@/app/organisms/BotProfileRenderer'
+'use client';
+import { useRouter } from 'next/navigation';
+import '../../style.css';
+import Navbar from '../../components/NavBar';
+import ModaleNotificationMobileModale from '../../modali/ModaleNotificationMobileModale';
+import MobileOptionFullScreenModal from '../../modali/MobileOptionFullScreenModal';
+import { useAppDispatch, useAppSelector } from '@/app/lib/hooks';
+import { useEffect, useState } from 'react';
+import { setNavbarPage } from '@/app/lib/slices/appStateSlice';
+import BotProfileRenderer from '@/app/organisms/BotProfileRenderer';
+import { persistor } from '@/app/lib/store'
 
 const UserBotPage = () => {
-    const pathname = typeof window !== 'undefined' ? window.location.pathname : '';
-    const isLogged = useAppSelector(state => state.userCredentials.userName.length > 0)    
-    const segments = pathname.split('/')
-    const router = useRouter()
+  const [isHydrated, setIsHydrated] = useState(false)
+  const pathname = typeof window !== 'undefined' ? window.location.pathname : ''
+  const isLogged = useAppSelector(state => state.userCredentials.userName.length > 0)
+  const segments = pathname.split('/')
+  const router = useRouter()
+  const dispatch = useAppDispatch()
 
-    const encodedName = segments[segments.length - 1]
-    
-    const userNameString = decodeURIComponent(encodedName);
+  const encodedName = segments[segments.length - 1]
+  const userNameString = decodeURIComponent(encodedName)
 
-    const userCredential = useAppSelector(state => 
-        state.sessionGeneratedAccounts.acc.find(account => account.name === userNameString)
-    )
+  const userCredential = useAppSelector(state =>
+    state.sessionGeneratedAccounts.acc.find(account => account.name === userNameString)
+  )
 
-    useEffect(() => {
-        if(!isLogged){
-            router.push('/')
-        }
-    }, [isLogged])
+  // Verifica se Redux-Persist ha idratato lo stato
+  useEffect(() => {
+    const checkPersistedState = async () => {
+      const hasHydrated = await persistor.getState().bootstrapped;
+      setIsHydrated(hasHydrated)
+    };
 
-    const dispatch = useAppDispatch();
+    checkPersistedState()
+  }, []);
 
-    useEffect(() => {
-        dispatch(setNavbarPage(20))
-        console.log(userNameString)
-    }, [dispatch]);
-
-    if (userCredential) {
-        return (
-            <div id="container bg-danger">
-                <ModaleNotificationMobileModale />
-                <MobileOptionFullScreenModal />
-                <Navbar />
-                <BotProfileRenderer user={userCredential} />
-            </div>
-        );
+  useEffect(() => {
+    if (!isLogged) {
+      router.push('/')
     }
+  }, [isLogged, router])
 
-}
+  useEffect(() => {
+    if (isHydrated && (userCredential || !isLogged)) {
+      dispatch(setNavbarPage(20));
+      console.log('Decoded User Name:', userNameString)
+    }
+  }, [dispatch, userCredential, isLogged, userNameString, isHydrated])
 
-export default UserBotPage
+  if (!isHydrated) {
+    return <div>Loading ...</div>
+  }
+
+  if (!isLogged) {
+    return <div>Redirecting...</div>
+  }
+
+  return (
+    <div id="container bg-danger">
+      <ModaleNotificationMobileModale />
+      <MobileOptionFullScreenModal />
+      <Navbar />
+      {userCredential ? (
+        <BotProfileRenderer user={userCredential} />
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default UserBotPage;
