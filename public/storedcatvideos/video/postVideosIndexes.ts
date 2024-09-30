06f3a599a7fc9f7b431752dc40f46f4de8332512
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-30 12:26:51 +0200

    new videos added

--- public/storedcatvideos/video/postVideosIndexes.ts
+++ public/storedcatvideos/video/postVideosIndexes.ts
@@ -1,5 +1,12 @@
 export const postVideoIndexes = [
+    '/storedcatvideos/video/634BC1C1B76CF36E73428808EC541FB6_video_dashinit.mp4',
+    '/storedcatvideos/video/714F51DEB0638D1AA36E700CCDE9039E_video_dashinit.mp4',
+    '/storedcatvideos/video/AQM_PdQFM-pEJ8F-bFsk44QfcTuf_F7F-zR-BzUGNUqLwzrf-5w1fiv2nZpMNSwXlgPVQIb0g5xbXx_rFdjZAp1G.mp4',
+    '/storedcatvideos/video/AQOopUkeR_95LlITyDZ8MyBRTuh3SgahPFT_7uUu_HyDf64riXvzA_EPTQf0QPXqst1iElqnSacZXZHioMWhdiFC.mp4',
+    '/storedcatvideos/video/AQOsd-4VxIg8uBV0Auo60w3oliHwupKcvAsMVbBgOi7qVzFO70xmEpMUGE-4MFYlp-DsPlCHJycEvBJ4MJUmhgo6.mp4',
     '/storedcatvideos/video/Cat Transcendence- limitless.mp4',
+    '/storedcatvideos/video/D94640F583D53D61DDE7BA3BAF4D2C94_video_dashinit.mp4',
+    '/storedcatvideos/video/D94672BAB9B2CF509E6BD196E3EC699F_video_dashinit.mp4',
     '/storedcatvideos/video/Snapinsta.app_video_3748F277C5DDEB939492A86565CD82B4_video_dashinit.mp4',
     '/storedcatvideos/video/Snapinsta.app_video_39490BA491D28416267FA7EDA978C8B1_video_dashinit.mp4',
     '/storedcatvideos/video/Snapinsta.app_video_534FDA6C1C4E92207D9CFF9F676B99A5_video_dashinit.mp4',
@@ -7,4 +14,5 @@ export const postVideoIndexes = [
     '/storedcatvideos/video/videoplayback (1).mp4',
     '/storedcatvideos/video/videoplayback (2).mp4',
     '/storedcatvideos/video/videoplayback.mp4',
+    '/storedcatvideos/video/youtube_-2hFF6pINUo_720x1280_h264.mp4',
 ];
