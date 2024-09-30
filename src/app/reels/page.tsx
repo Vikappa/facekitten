ffce6c12612b16d698164a1395eb557a48ec81cb
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-09-30 12:39:48 +0200

    small fix

--- src/app/reels/page.tsx
+++ src/app/reels/page.tsx
@@ -6,18 +6,25 @@ import '../style.css'
 import VideoPageOrg from "../organisms/ViodePageOrg"
 import { useAppDispatch, useAppSelector } from "../lib/hooks"
 import { setNavbarPage, setShowNotificationModal } from "../lib/slices/appStateSlice"
+import { useRouter } from "next/navigation"
 
 const ReelPage = () => {
 
     const [showMobileSearch, setShowMobileSearch] = useState<boolean>(false)
     const dispatch = useAppDispatch()
     const shownPage = useAppSelector(state => state.status.shownpage)
+    const isLoggeId = useAppSelector(state => state.userCredentials.userName.length > 0)
+    const router = useRouter()
 
     useEffect(() => {
       if(shownPage !== 10) {
         setShowMobileSearch(false)
         dispatch(setShowNotificationModal(false))
         dispatch(setNavbarPage(10))
+      }
+
+      if(!isLoggeId){
+        router.push('/') 
       }
     }, [])
     
