91dae6f4dca71a626a4aac575fba5e36a55b1430
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2025-10-30 21:08:35 +0100

    Media added

---
+++ public/storedcatcoverphotos/coverPhotoIndexes.ts
@@ -0,0 +1,9 @@
+export const coverPhotoIndexes = [
+    '/storedcatcoverphotos/199dda88fbca22eae7b3c5f3c022e16b.jpg',
+    '/storedcatcoverphotos/2a2148f68d29e2f8177528df8c2064b3--cover-photos-for-facebook-facebook-timeline.jpg',
+    '/storedcatcoverphotos/Beautiful-Black-Cat-Facebook-Covers-FBcoverlover_facebook_cover.jpg',
+    '/storedcatcoverphotos/Cat-Sunglasses-Facebook-Covers-1670.jpg',
+    '/storedcatcoverphotos/Cool-Cat-Facebook-Profile-Timeline-Cover-Ultimate-Collection-Of-Top-50-Best-Cute-Facebook-Cover-For1.jpg',
+    '/storedcatcoverphotos/d8d1b4edc273553a807e856aca7ac1b6.jpg',
+    '/storedcatcoverphotos/grumpy-cat-background-facebook-cover.jpg',
+];
