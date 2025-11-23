d01040d57e921cfa1273ad250bfd7bd19714534b
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2025-11-23 01:05:30 +0100

    focus:ring-[0.9px] sempre

--- src/app/components/atoms/ResponsiveLoginForm.tsx/InputLoginForm.tsx
+++ src/app/components/atoms/ResponsiveLoginForm.tsx/InputLoginForm.tsx
@@ -14,7 +14,7 @@ export function InputLoginForm({ id, type, placeholder, value, onChange }: Input
             placeholder={placeholder}
             value={value}
             onChange={onChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 py-4 lg:focus:ring-[0.9px]"
+            className="w-full px-3 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 py-4 focus:ring-[0.9px]"
         />
     )
 }
